2c5949a98c69a43add94436836c5e4f36965878d
goodluck04 <goodluckkumar04@gmail.com>
2023-11-03 10:16:52 +0530

    third commit

--- components/contact.tsx
+++ components/contact.tsx
@@ -27,7 +27,7 @@ export default function Contact() {
     <motion.section
       id="contact"
       ref={ref}
-      className="mb-20 sm:mb-28 w-[min(100%,38rem)] text-center"
+      className="mb-20 sm:mb-28 w-[min(100%,38rem)] text-center mt-16 scroll-mt-24"
       initial={{
         opacity: 0,
       }}
