15273196f003b537e585aeb031b9e6be73b89099
goodluck04 <goodluckkumar04@gmail.com>
2023-11-03 11:39:00 +0530

    add small changes commit

--- components/footer.tsx
+++ components/footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <footer className="mb-10 mt-10y px-4 text-center py-20 scroll-mt-  text-gray-500">
       <small className="mb-2 block text-xs">
-        &copy; 2030 Goodluck. All rights reserved.
+        &copy; 2023 Goodluck. All rights reserved.
       </small>
       <p className="text-xs">
         <span className="font-semibold">About this website:</span> built with
